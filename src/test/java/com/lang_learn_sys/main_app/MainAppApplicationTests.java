4c0ad69a7c50af028e1609fcadf017b0dc8cb857
Akarin <rikale.aw@gmail.com>
2020-05-18 17:26:43 +0300

    Initial commit

---
+++ src/test/java/com/lang_learn_sys/main_app/MainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lang_learn_sys.main_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
